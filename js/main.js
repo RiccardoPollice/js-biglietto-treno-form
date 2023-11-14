995eb62e169a3596a8d6943009ece05c2a1730ca
RiccardoPollice <riccardo.pollice17@gmail.com>
2023-11-14 16:06:04 +0100

    Lavoro su Java Script

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function dataUser(event){
+    event.preventDefault();
+    let name = document.getElementById("name").value;
+    let distance = document.getElementById("Km_da_Percorrere").value;
+    let age = document.getElementById("age_select").value;
+
+    distance = parseInt(distance);
+    let ticketPrice = distance *0.21;
+
+    if (age == "Minorenne") {
+        ticketPrice = ticketPrice * 0.8;
+    }
+    else if (age == "Over65") {
+        ticketPrice = ticketPrice * 0.6
+    }
+    let finalprice = ticketPrice.toFixed(2);
+
+
+
+    console.log(finalprice)
+    document.getElementById("user_price").innerHTML = finalprice
+
+
+    
+}
